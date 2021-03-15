ba43337e337a389193ef820fcb201d2bbabc367d
chad svastisalee <swasgrafx@gmail.com>
2021-03-15 17:44:56 -0500

    Implemented an article view

---
+++ src/app/article-view/article-view.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ArticlesService } from '../articles.service';
+import { Article } from '../article';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-article-view',
+  templateUrl: './article-view.component.html',
+  styleUrls: ['./article-view.component.scss']
+})
+export class ArticleViewComponent implements OnInit {
+
+  article: Article;
+
+  constructor(
+    private articlesService: ArticlesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.getArticle(id);
+  }
+
+  getArticle(id): void {
+    this.articlesService.getArticle(id).subscribe(
+      (response: any) => {
+        this.article = response.article
+      }
+    );
+  }
+  
+  
+}
