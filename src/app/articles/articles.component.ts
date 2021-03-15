2609391e5060d954c9f918f40fe841c22a6629e9
chad svastisalee <swasgrafx@gmail.com>
2021-03-15 17:24:21 -0500

    Implemented an articles UI

--- src/app/articles/articles.component.ts
+++ src/app/articles/articles.component.ts
@@ -27,9 +27,9 @@ export class ArticlesComponent implements OnInit {
   // 5. Create a local wrapper for
   getArticles(): void {
     this.articlesService.getArticles().subscribe(
-      (response) => {
-        this.articles = response,
-        console.log(this.articles)
+      (response:any) => {
+        this.articles = response.articles
+        // console.log(this.articles)
       }
     );
   }
